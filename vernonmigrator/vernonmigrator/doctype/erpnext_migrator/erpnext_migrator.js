98193dfad29a19dbaec04fad77ed3a261c3a53ff
erickmo <mo@thunderlab.id>
2025-02-15 07:49:46 +0000

    update doctype migrator

--- vernonmigrator/vernonmigrator/doctype/erpnext_migrator/erpnext_migrator.js
+++ vernonmigrator/vernonmigrator/doctype/erpnext_migrator/erpnext_migrator.js
@@ -5,24 +5,97 @@ frappe.ui.form.on("ERPNext Migrator", {
 	refresh(frm) {
 
 	},
-	import_item_group: function (frm) { new ERPNextMigratorController(frm).import_now("Item Group"); },
-	import_item: function (frm) { new ERPNextMigratorController(frm).import_now("Item"); },,
-	import_address: function (frm) { new ERPNextMigratorController(frm).import_now("Address"); },,
-	import_contact: function (frm) { new ERPNextMigratorController(frm).import_now("Contact"); },,
-	import_customer_group: function (frm) { new ERPNextMigratorController(frm).import_now("Customer Group"); },,
-	import_customer: function (frm) { new ERPNextMigratorController(frm).import_now("Customer"); },,
-	import_supplier_group: function (frm) { new ERPNextMigratorController(frm).import_now("Supplier Group"); },,
-	import_supplier: function (frm) { new ERPNextMigratorController(frm).import_now("Supplier"); },,
-	import_account: function (frm) { new ERPNextMigratorController(frm).import_now("Account"); },,
-	import_journal_entry: function (frm) { new ERPNextMigratorController(frm).import_now("Journal Entry"); },,
-	import_purchase_order: function (frm) { new ERPNextMigratorController(frm).import_now("Purchase Order"); },,
-	import_purchase_invoice: function (frm) { new ERPNextMigratorController(frm).import_now("Purchase Invoice"); },,
-	import_purchase_receipt: function (frm) { new ERPNextMigratorController(frm).import_now("Purchase Receipt"); },,
-	import_sales_order: function (frm) { new ERPNextMigratorController(frm).import_now("Sales Order"); },
-	import_sales_invoice: function (frm) { new ERPNextMigratorController(frm).import_now("Sales Invoice"); },,
-	import_payment_entry: function (frm) { new ERPNextMigratorController(frm).import_now("Payment Entry"); },,
-	import_stock_entry: function (frm) { new ERPNextMigratorController(frm).import_now("Stock Entry"); },,
-	import_delivery_note: function (frm) { new ERPNextMigratorController(frm).import_now("Delivery Note"); },
+	import_item_group: function (frm) { 
+		// Confirm
+		frappe.confirm("Are you sure you want to import Item Group?", function () {
+			new ERPNextMigratorController(frm).import_now("Item Group"); 
+		})
+	},
+	import_item: function (frm) { 
+		frappe.confirm("Are you sure you want to import Item?", function () {
+			new ERPNextMigratorController(frm).import_now("Item"); 
+		});
+	},
+	import_address: function (frm) { 
+		frappe.confirm("Are you sure you want to import Address?", function () {
+			new ERPNextMigratorController(frm).import_now("Address"); 
+		})
+	},
+	import_contact: function (frm) { 
+		frappe.confirm("Are you sure you want to import Contact?", function () {
+			new ERPNextMigratorController(frm).import_now("Contact"); 
+		});
+	},
+	import_customer_group: function (frm) { 
+		frappe.confirm("Are you sure you want to import Customer Group?", function () {
+			new ERPNextMigratorController(frm).import_now("Customer Group"); 
+		});
+	},
+	import_customer: function (frm) { 
+		frappe.confirm("Are you sure you want to import Customer?", function () {
+			new ERPNextMigratorController(frm).import_now("Customer"); 
+		});
+	},
+	import_supplier_group: function (frm) { 
+		frappe.confirm("Are you sure you want to import Supplier Group?", function () {
+			new ERPNextMigratorController(frm).import_now("Supplier Group"); 
+		});
+	},
+	import_supplier: function (frm) { 
+		frappe.confirm("Are you sure you want to import Supplier?", function () {
+			new ERPNextMigratorController(frm).import_now("Supplier"); 
+		});
+	},
+	import_account: function (frm) { 
+		frappe.confirm("Are you sure you want to import Account?", function () {
+			new ERPNextMigratorController(frm).import_now("Account"); 
+		});
+	},
+	import_journal_entry: function (frm) { 
+		frappe.confirm("Are you sure you want to import Journal Entry?", function () {
+			new ERPNextMigratorController(frm).import_now("Journal Entry"); 
+		});
+	},
+	import_purchase_order: function (frm) { 
+		frappe.confirm("Are you sure you want to import Purchase Order?", function () {
+			new ERPNextMigratorController(frm).import_now("Purchase Order"); 
+		});
+	},
+	import_purchase_invoice: function (frm) { 
+		frappe.confirm("Are you sure you want to import Purchase Invoice?", function () {
+			new ERPNextMigratorController(frm).import_now("Purchase Invoice"); 
+		});		
+	},
+	import_purchase_receipt: function (frm) { 
+		frappe.confirm("Are you sure you want to import Purchase Receipt?", function () {
+			new ERPNextMigratorController(frm).import_now("Purchase Receipt"); 
+		});		
+	},
+	import_sales_order: function (frm) { 
+		frappe.confirm("Are you sure you want to import Sales Order?", function () {
+			new ERPNextMigratorController(frm).import_now("Sales Order"); 
+		});
+	},
+	import_sales_invoice: function (frm) { 
+		frappe.confirm("Are you sure you want to import Sales Invoice?", function () {
+			new ERPNextMigratorController(frm).import_now("Sales Invoice"); 
+		});
+	},
+	import_payment_entry: function (frm) { 
+		frappe.confirm("Are you sure you want to import Payment Entry?", function () {
+			new ERPNextMigratorController(frm).import_now("Payment Entry"); 
+		});
+	},
+	import_stock_entry: function (frm) { 
+		frappe.confirm("Are you sure you want to import Stock Entry?", function () {
+			new ERPNextMigratorController(frm).import_now("Stock Entry"); 
+		});
+	},
+	import_delivery_note: function (frm) { 
+		frappe.confirm("Are you sure you want to import Delivery Note?", function () {
+			new ERPNextMigratorController(frm).import_now("Delivery Note"); 
+		});
+	},
 });
 
 class ERPNextMigratorController {
